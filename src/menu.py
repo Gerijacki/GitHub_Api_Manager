ca67b23383d198607962b609314e652064ecdff4
Gerijacki <gerardloriz05@gmail.com>
2025-02-03 22:19:40 +0100

    base:feat: most basic app

--- src/menu.py
+++ src/menu.py
@@ -1,50 +1,14 @@
-import sys
 from src.Github import GitHub
-from src.modules.RepoManager import RepoManager
-from src.modules.OrgManager import OrgManager
-from src.modules.StatsManager import StatsManager
-from src.modules.OrgMenu import OrgMenu
-from src.modules.RepoMenu import RepoMenu
-from src.modules.StatsMenu import StatsMenu
-from src.modules.Utils import clear_screen, pause, print_error, print_info, print_success, print_warning
+from src.modules.Utils import *
 
 class Menu:
     def __init__(self):
         self.github = GitHub()
-        self.repo_manager = RepoManager()
-        self.org_manager = OrgManager()
-        self.stats_manager = StatsManager()
-        self.org_menu = OrgMenu(self.org_manager)
-        self.stats_menu = StatsMenu(self.stats_manager)
-        self.repo_menu = RepoMenu(self.repo_manager)
 
     def mostrar_menu(self):
-        while True:
-            clear_screen()
-            print("\n==== GESTOR DE GITHUB ====")
-            print_info("1. Veure informació de l'usuari [ 🚻 ]")
-            print_info("2. Gestionar Repos [ 💭 ]")
-            print_info("3. Gestionar Organitzacions [ 🗄️ ]")
-            print_info("4. Veure estadístiques d'un repositori [ 💱 ]")
-            print_info("5. Salir [ 🏳️ ]")
-
-            opcion = input("\nSel·lecciona una opció: ")
-
-            if opcion == "1":
-                self.ver_usuario()
-            elif opcion == "2":
-                self.repo_menu.gestionar_repo()
-            elif opcion == "3":
-                self.org_menu.gestionar_organizaciones()
-            elif opcion == "4":
-                self.stats_menu.ver_stats_repo()
-            elif opcion == "5":
-                print_warning("Sortint...")
-                sys.exit()
-            else:
-                print_error("\nOpció invàlida.")
-
-            pause()
+        clear_screen()
+        self.ver_usuario()
+        pause()
 
     def ver_usuario(self):
         user = self.github.get_user()
